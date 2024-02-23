6858b5a6aa7de8a9683628b9b9170d62960f4f95
Mahib09 <Mahib.Ranamagar@triosstudent.com>
2024-02-23 14:15:11 -0500

    login and crud functionality added

--- crudoperation_login_mahib/src/main/java/com/example/crudoperation_login_mahib/HelloController.java
+++ crudoperation_login_mahib/src/main/java/com/example/crudoperation_login_mahib/HelloController.java
@@ -47,8 +47,100 @@ public class HelloController implements Initializable{
         tblColStatus.setCellValueFactory(new PropertyValueFactory<TaskRecord,String>("task_status"));
         tblView.setItems(list);
     }
-    String jdbcUrl = "jdbc:mysql://localhost:3306/csd214_mahib_test2(testday)";
+    String jdbcUrl = "jdbc:mysql://localhost:3306/csd214_mahib_test2(testday))";
     String dbUser = "root";
     String dbPassword = "";
+    @FXML protected void onReadButtonClicked() {
+        populateTable();
+    }
+    @FXML protected void onInsertButtonClicked(){ insertData(); }
+    @FXML protected void onUpdateButtonClicked() { updateData(); }
+    @FXML protected void onDeleteButtonClicked(){ deleteData();}
+    @FXML protected void onLoadButtonClicked(){loadData();}
+
+    public void setWelcomeText(String Name){
+        welcomeText.setText("Hello, "+ Name+"!" );
+    }
+    public void populateTable() {
+        list.clear();
+        try (Connection connection = DriverManager.getConnection(jdbcUrl, dbUser, dbPassword)) {
+            String query = "SELECT * FROM task_management";
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                int tblId = resultSet.getInt("task_id");
+                String tblName = resultSet.getString("task_name");
+                String tblDescription = resultSet.getString("task_description");
+                String tblStatus = resultSet.getString("task_status");
+                tblView.getItems().add(new TaskRecord(tblId,tblName,tblDescription,tblStatus));
+            }     } catch (SQLException e) {
+            e.printStackTrace();     }
+    }
+    public void insertData(){
+        if(fieldName.getText().isEmpty()){
+            lblMessage.setText("Name Required to Insert");
+        }else{
+            try (Connection connection = DriverManager.getConnection(jdbcUrl, dbUser, dbPassword)) {
+                String query = "INSERT INTO task_management ( task_name, task_description, task_status) VALUES ('"+fieldName.getText()+"','"+fieldDescription.getText()+"','"+fieldStatus.getText()+"')";
+                Statement statement = connection.createStatement();
+                statement.executeUpdate(query);
+                populateTable();
+                clearForm();
+            } catch (SQLException e) {
+                e.printStackTrace();     }
+        }
+
+    }
+    public void updateData(){
+        if(fieldId.getText().isEmpty()){
+            lblMessage.setText("Please Enter Id to Update");
+        }else{
+            try (Connection connection = DriverManager.getConnection(jdbcUrl, dbUser, dbPassword)) {
+                String query = ("UPDATE task_management SET task_name='" + fieldName.getText() + "',task_description='" + fieldDescription.getText() + "',task_status='" + fieldStatus.getText() + "'WHERE task_id='" + fieldId.getText() + "'");
+                Statement statement = connection.createStatement();
+                statement.executeUpdate(query);
+                populateTable();
+                clearForm();
+            } catch (SQLException e) {
+                e.printStackTrace();     }
+        }
+
+    }
+    public void deleteData(){
+        if(fieldId.getText().isEmpty()){
+            lblMessage.setText("Please Enter Id to Delete");
+        }else{ try (Connection connection = DriverManager.getConnection(jdbcUrl, dbUser, dbPassword)) {
+            String query = ("DELETE FROM task_management WHERE task_id='"+fieldId.getText()+"'");
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(query);
+            populateTable();
+            clearForm();
+        } catch (SQLException e) {
+            e.printStackTrace();     }}
+
+    }
+    public void loadData(){
+        if(fieldId.getText().isEmpty()){
+            lblMessage.setText("Please Enter Id to Load");
+        }else{try (Connection connection = DriverManager.getConnection(jdbcUrl, dbUser, dbPassword)) {
+            String query = ("SELECT * FROM task_management WHERE task_id='"+fieldId.getText()+"'");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            resultSet.next();
+            fieldName.setText(resultSet.getString("task_name"));
+            fieldDescription.setText(resultSet.getString("task_description"));
+            fieldStatus.setText(resultSet.getString("task_status"));
+            lblMessage.setText("");
+        } catch (SQLException e) {
+            e.printStackTrace();     }}
+
+    }
+    public void clearForm(){
+        fieldId.setText("");
+        fieldName.setText("");
+        fieldDescription.setText("");
+        fieldStatus.setText("");
+        lblMessage.setText("");
+    }
 
 }
--- crudoperation_login_mahib/src/main/java/com/example/crudoperation_login_mahib/LoginController.java
+++ crudoperation_login_mahib/src/main/java/com/example/crudoperation_login_mahib/LoginController.java
@@ -22,18 +22,57 @@ public class LoginController {
     @FXML
     private Label loginMessage;
 
-    String jdbcUrl = "jdbc:mysql://localhost:3306/csd214_mahib_test2";
+    String jdbcUrl = "jdbc:mysql://localhost:3306/csd214_mahib_test2(testday))";
     String dbUser = "root";
     String dbPassword = "";
     @FXML
    protected void onLoginClick(){
+        String Entered_email=loginUsername.getText();
+        String Entered_password=loginPassword.getText();
+        try {
+            Connection connection = DriverManager.getConnection(jdbcUrl, dbUser, dbPassword);
+            String query="SELECT * FROM user_info Where user_email='"+Entered_email+"' and user_password='"+Entered_password+"'";
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            if(resultSet.next()){
+                String Name=resultSet.getString("user_name");
+                try {
+                    // Load the FXML file for the second scene
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+                    Parent secondScene = loader.load();
+
+                    // Access the controller of the second scene
+                    HelloController helloController = loader.getController();
+
+                    // Set the data in the controller of the second scene
+                    helloController.setWelcomeText(Name);
+
+                    // Create a new stage for the second scene
+                    Stage secondStage = new Stage();
+                    secondStage.setTitle("User Scene");
+                    secondStage.setScene(new Scene(secondScene));
+
+                    // Close the first scene's stage
+                    Stage firstSceneStage = (Stage) loginUsername.getScene().getWindow(); // Assuming hbt is a node in the first scene
+                    firstSceneStage.close();
+
+                    // Show the second stage
+                    secondStage.show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }else{
+                loginMessage.setText("Invalid UserName or Password");
+            }
+        }catch (SQLException e) {
+            e.printStackTrace();     }
 
     }
-
-
-
-
-
 }
+
+
+
+
+
 
 
